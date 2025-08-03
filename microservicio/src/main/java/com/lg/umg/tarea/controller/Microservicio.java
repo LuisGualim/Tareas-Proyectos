367b106bb2cc392234ffe68d7e9599f139e0ce43
LuisGualim <gualimisem@gmail.com>
2025-08-03 12:02:56 -0600

    Microservicio básico con CRUD simple

--- microservicio/src/main/java/com/lg/umg/tarea/controller/Microservicio.java
+++ microservicio/src/main/java/com/lg/umg/tarea/controller/Microservicio.java
@@ -1,5 +1,6 @@
 package com.lg.umg.tarea.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,26 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/microservicio")
+@RequestMapping("/api/demo")
 public class Microservicio {
 
 	@GetMapping("/get")
-	public String getMicroservicio() {
-		return "Se  utilizo el metodo GET";
+	public ResponseEntity<String> get() {
+		return ResponseEntity.ok ("Se  utilizo el metodo GET");
 	}
 	
 	@PostMapping("/post")
-	public String postMicroservicio() {
-		return "Se  utilizo el metodo POST";
+	public ResponseEntity<String> post() {
+		return ResponseEntity.ok ("Se  utilizo el metodo POST");
 	}
 	
 	@PutMapping("/put")
-	public String putMicroservicio() {
-		return "Se  utilizo el metodo PUT";
+	public ResponseEntity<String> put() {
+		return ResponseEntity.ok ("Se  utilizo el metodo PUT");
 	}
 	
 	@DeleteMapping("/delete")
-	public String deleteMicroservicio() {
-		return "Se  utilizo el metodo DELETE";
+	public ResponseEntity<String> delete() {
+		return ResponseEntity.ok ("Se  utilizo el metodo DELETE");
 	}
 }
